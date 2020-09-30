917b8930eebfc415c8dbda342d0d5cbbad9872cf
Logan Velasco <logan@bcbsil.com>
2020-09-30 09:33:15 -0500

    2nd feature commit

--- core/src/com/logan/obstacleavoid/screen/game/GameScreen.kt
+++ core/src/com/logan/obstacleavoid/screen/game/GameScreen.kt
@@ -26,6 +26,7 @@ class GameScreen(val game: ObstacleAvoidGame) : Screen{
 
         controller = GameController()
         renderer = GameRenderer(controller, assetManager)
+
     }
 
     override fun render(delta: Float) {
